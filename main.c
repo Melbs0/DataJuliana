690fce779d5c72291d47678176a49b433755d535
melbs <95056990+Melbs0@users.noreply.github.com>
2023-05-29 11:44:29 -0300

    Add files via upload
    starts thing

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int dia, mes,ano;
+	
+	printf("%d",dj(2023, 5,26));
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+
+
+int dj(int Ano, int Mes, int Dia){
+	int a, b,c,d,e,dj;
+	
+	if(Mes<3){
+		Ano--;
+		Mes +=12;	
+	}
+	
+	if(Ano <=1582 && Dia <= 15 && Mes <=10 ){
+		
+		a =(int)floor(Ano/100);
+		b= (int)floor(a/4);
+		c = 2-a+b;
+		}
+	else{
+		c=0;
+	}
+	
+	d =(int) floor(365.25*(Ano+4716));
+	e = (int) floor(30.6001*(Mes++));
+	
+	dj = d+e+Dia+c-1524;
+	
+	return dj;
+	
+	
+}
